13234a2ab1d4561af480bd27e28bcf77a1f618ed
Alex Hardwicke <alex.hardwicke@outlook.com>
2015-09-13 22:39:52 +0200

    Initial commit
    Commit of repo demoing bug

---
+++ FSharpLibrary/PortableLibrary1.fs
@@ -0,0 +1,28 @@
+﻿namespace FSharpLibrary
+
+type DemoTypeA = {
+    Property: int;
+    AnotherProperty: string;
+}
+
+type DemoTypeB = {
+    Data: string list;
+    Message: string;
+}
+
+type CompositeType = {
+    A: DemoTypeA;
+    B: DemoTypeB;
+}
+
+type Serialise() = 
+    member this.X =
+        let a = { Property = 0
+                  AnotherProperty = "hello" }
+        let b = { Data = []
+                  Message = "a message"
+                }
+        let c = { A = a; B = b }
+
+        let serialised = c |> Newtonsoft.Json.JsonConvert.SerializeObject
+        serialised
---
+++ README.md
@@ -0,0 +1,3 @@
+# Demonstrates a null reference in JSON.Net that occurs with a certain configuration
+
+Reproduced on both Windows 8.1 and Windows 10.
